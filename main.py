f644b77c66a5fa644e74f67abfc7e971d260859b
Thibault231 <salgues231@gmail.com>
2020-01-16 15:17:19 +0100

    Add events pseudocode and song fonction
    adding songs directory.

--- main.py
+++ main.py
@@ -8,22 +8,23 @@ from class_character import *
 from class_element import *
 
 def main():
+	
 	# create map, items and characters with their positions
 	map_game = Map(15, 15)
 	list_items = items_settlement()
 	MacGuffin =list_items.pop()
 	mac_giver, guardian = characters_settlement(MacGuffin)
-
-	
 	# create and display the pygame main window surface	
 	pygame.init()
 	window_resolution = (600, 600)
-	launched = True
-
 	pygame.display.set_caption("Labyrinthe Mac Giver")
 	window_surface = pygame.display.set_mode(window_resolution, \
 	pygame.RESIZABLE)   # surface object
 	pygame.display.flip()
+
+	#creates starts playlist songs.
+	pygame.mixer.music.load("songs/InClosing-DaysPast.mp3")
+	pygame.mixer.music.play()
 
 	# display labyrinth, items and characters on the main surface
 	map_game.show_labyrinth(window_surface)
@@ -32,14 +33,45 @@ def main():
 	mac_giver.show_element(window_surface)
 	guardian.show_element(window_surface)
 
+	# execute the game script for events
+	launched = True
+	
 	while launched:
+	    
 	    for event in pygame.event.get():
+	        
 	        if event.type == pygame.QUIT:
 	            launched = False
-	    
-		#pygame.display.flip()
+	        if event.type == KEYDOWN:
+				if event.key == K_DOWN:
+					#Colisions cheking
+					#MacGyver movement
+					#running interractions with items and guardian
+					pass
+				elif event.key == K_UP:
+					#Colisions cheking
+					#MacGyver movement
+					#running interractions with items and guardian
+					pass
+				elif event.key == K_RIGHT:
+					#Colisions cheking
+					#MacGyver movement
+					#running interractions with items and guardian
+					pass
+				elif event.key == K_UP:
+					#Colisions cheking
+					#MacGyver movement
+					#running interractions with items and guardian
+					pass
+				else:
+					pass
+		
+		pygame.display.flip()
+	
+	pygame.mixer.music.stop()
+	pygame.quit()
 
-
+		
 main()
 
 
